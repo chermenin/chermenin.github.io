0081a01bf8573e8984c229d83e2d1092f5eeed13
Alexander Chermenin <alex@chermenin.ru>
2014-09-29 15:29:44 +0600

    Updated template.

---
+++ js/slider.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+
+    $('#values_list li').hover(function(){
+     $('#values_list li').removeClass('active');
+        var item = $(this);
+          item.addClass('active');
+          $('#user_title').html(item.attr('data-title'));
+          $('#user_value').html(item.attr('data-value'));
+          
+          if(item.attr('data-caps')){
+            $('#user_value').css({'text-transform': 'lowercase'});
+          } else {
+            $('#user_value').css({'text-transform': 'capitalize'});
+          }
+    });
+});
+
